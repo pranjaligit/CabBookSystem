f5d5f05d011605a7db918f5ecab3ff169130fc4a
pranjaligit <pranjalikulkarni13@gmail.com>
2021-09-17 17:10:05 +0530

    first commit by pranjali

---
+++ src/test/java/com/cg/cabapi/CabapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.cabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
